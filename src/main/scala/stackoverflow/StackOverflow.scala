dbcfb05a0ef0771211cb46c949a4c53a38cee53f
Jorge Pardavila <jorge@pardavila.com>
2021-02-01 13:08:17 +0100

    cambios en master

--- src/main/scala/stackoverflow/StackOverflow.scala
+++ src/main/scala/stackoverflow/StackOverflow.scala
@@ -31,10 +31,11 @@ object StackOverflow extends StackOverflow {
     val means   = kmeans(sampleVectors(vectors), vectors, debug = true)
     val results = clusterResults(means, vectors)
     printResults(results)
-    println("Esto es una pruebaauuaaaa 23" +
+    println("Esto es una pruebaautttuaaaa 23 " +
       "gg1gg" +
-      "g25gg  1122111" +
-      "rr2r  55")
+      "g25gg  1122111 cacaca" +
+      " rr2r  5 5" +
+      "rge")
   }
 }
 
